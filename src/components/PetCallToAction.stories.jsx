66a826429f91733fdc19bbc7793d86c54d572119
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-10-22 21:02:24 -0500

    created a storybook for PetCallToAction.js file

---
+++ src/components/PetCallToAction.stories.jsx
@@ -0,0 +1,19 @@
+import PetCallToAction from './PetCallToAction';
+
+const meta = {
+  component: PetCallToAction,
+};
+
+export default meta;
+
+export const Default = {
+  args: {
+    ctaText: "Call To Action Text"
+  }
+};
+
+export const AccessNow = {
+  args: {
+    ctaText: "Access Now!"
+  }
+};
