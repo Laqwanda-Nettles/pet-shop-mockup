bc5220f3dcae150beaa498ffddbe7bdf86b31635
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-10-22 22:20:13 -0500

    created a new story for the PetHeroText component

---
+++ src/components/PetHeroText.stories.jsx
@@ -0,0 +1,14 @@
+import PetHeroText from './PetHeroText';
+
+const meta = {
+  component: PetHeroText,
+};
+
+export default meta;
+
+export const Default = {
+  args: {
+    title: "Bring happiness to your home",
+    description: "Find the ideal pet for your lifestyle!"
+  }
+};
