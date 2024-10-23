0d85f1aad7f10ece8fd96fc60afc1736f8532328
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-10-22 22:18:58 -0500

    changed border radius and added margin bottom

--- src/components/PetCallToAction.js
+++ src/components/PetCallToAction.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 export default function PetCallToAction(props) {
   const { ctaText } = props;
   return (
-    <div className="bg-rose-600 p-3 flex justify-center items-center rounded-md">
+    <div className="bg-rose-600 mb-4 p-3 flex justify-center items-center rounded-xl">
       <button className="bg-white p-3 m-2 w-4/5 text-blue-950 font-semibold rounded-lg">
         {ctaText}
       </button>
