d3e20e202e3c49735f4147c1546510710abec786
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-10-22 21:07:11 -0500

    added a src prop and added it to img tag + styled component with tailwind

--- src/components/PetHeroImage.js
+++ src/components/PetHeroImage.js
@@ -1,15 +1,16 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 // TODO: in storybook, create a story for this component
 // update the value for src in the story
 // The image is located in public/img/hero-dog.png
-export default function PetHeroImage (props) {
+export default function PetHeroImage(props) {
+  const { src } = props;
   return (
-    <div>
-      TODO: Finish this component based on the design in public/mockups/hero-image.png
+    <div className="bg-gradient-to-b from-rose-400 to-rose-600 rounded-2xl">
+      <img src={src} alt="dog hero image" />
     </div>
   );
-};
+}
 
 PetHeroImage.propTypes = {
   src: PropTypes.string.isRequired,
